351b0f8ed11d1408f5fdd7b3918d21c45fcc4dca
mbrad26 <mbrad26@gmail.com>
2020-06-19 15:51:53 +0100

    Implemented taking turns

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function Square(props) {
   return (
     <button
       className="square"
-      onClick={() => props.onClick()}
+      onClick={props.onClick}
       >
       {props.value}
     </button>
@@ -18,12 +18,17 @@ class Board extends React.Component {
     super(props)
     this.state = {
       squares: Array(9).fill(null),
-    }
+      xIsNext: true,
+    };
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
